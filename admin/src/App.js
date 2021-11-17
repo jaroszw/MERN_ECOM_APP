319e51509ed0b9c54ceec36ef705a138ad270bdb
Wojciech Jarosz <jaroszw@gmail.com>
2021-11-17 09:45:30 +0100

    edit user page

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,6 +2,7 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Homepage from "./pages/Homepage";
 import UserList from "./pages/userList/userList.jsx";
+import User from "./pages/user/User.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:id" element={<User />} />
         </Routes>
       </div>
     </Router>
