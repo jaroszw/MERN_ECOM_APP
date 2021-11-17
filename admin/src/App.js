f8b3ae883b94fd8f2fad82311ebcbd36d7579018
Wojciech Jarosz <jaroszw@gmail.com>
2021-11-17 08:03:25 +0100

    user panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,22 @@
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Homepage from "./pages/Homepage";
+import UserList from "./pages/userList/userList.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Homepage />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- admin/src/dummyData.js
+++ admin/src/dummyData.js
@@ -1,11 +1,95 @@
 export const userData = [
-  { name: 'Jan', 'Active Users': 4000 },
-  { name: 'Feb', 'Active Users': 2400 },
-  { name: 'Mar', 'Active Users': 3600 },
-  { name: 'Apr', 'Active Users': 7200 },
-  { name: 'May', 'Active Users': 8100 },
-  { name: 'Jun', 'Active Users': 4500 },
-  { name: 'July', 'Active Users': 9000 },
-  { name: 'Oct', 'Active Users': 11000 },
-  { name: 'Sep', 'Active Users': 7500 },
+  { name: "Jan", "Active Users": 4000 },
+  { name: "Feb", "Active Users": 2400 },
+  { name: "Mar", "Active Users": 3600 },
+  { name: "Apr", "Active Users": 7200 },
+  { name: "May", "Active Users": 8100 },
+  { name: "Jun", "Active Users": 4500 },
+  { name: "July", "Active Users": 9000 },
+  { name: "Oct", "Active Users": 11000 },
+  { name: "Sep", "Active Users": 7500 },
+];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 2,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 3,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 4,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 5,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 6,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 7,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 8,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  {
+    id: 9,
+    username: "Jon Snow",
+    avatar:
+      "https://houseofpostcards.pl/userdata/public/gfx/522c28ecbe96f11a9c344e6462f92070.jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
 ];
